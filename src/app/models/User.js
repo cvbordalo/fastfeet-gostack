ee92a26f199aa1d53aee53b68aeba93f6d46f1c8
Carlos Bordalo <carlosvitor6@hotmail.com>
2020-02-20 02:34:12 -0300

    feat: Add User Registration, UserController.js. Also fix a bug at User.js in the User class

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -5,21 +5,12 @@ import Sequelize, { Model } from 'sequelize';
 class User extends Model {
   // O método abaixo (init) será chamado automaticamente pelo sequelize
   static init(sequelize) {
-    /*
-      "Super" indica que estamos usando a classe "model",
-      e na sequencia usando o init do model
-      Para o init, enviamos as colunas que serão inseridas pelo usuário,
-      o que é chave estrangeira ou preenchido automaticamente, logo, não precisa
-      de Sequelize. VIRTUAL é um campo que será gerado apenas no lado da aplicação,
-      não será gerado na base de dados
-    */
     super.init(
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
-        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
-        type_acess: Sequelize.INTEGER,
+        provider: Sequelize.BOOLEAN,
       },
       {
         sequelize,
