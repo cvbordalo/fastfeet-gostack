f383b8ff57ca01bc2c16ddd52bb67f24971cab47
Carlos Bordalo <carlosvitor6@hotmail.com>
2020-02-23 04:00:05 -0300

    feat: Add Recipients Migration

---
+++ src/app/controllers/RecipientController.js
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken';
+
+import Recipient from '../models/Recipient';
+
+class RecipientController {
+  async store(req, res) {}
+}
+
+export default new RecipientController();
---
+++ src/database/migrations/20200223065057-create-recipients.js
@@ -0,0 +1,60 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('recipients', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      number: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      complement: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      state: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      zip_code: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      created_by_user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      updated_by_user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('recipients');
+  },
+};
